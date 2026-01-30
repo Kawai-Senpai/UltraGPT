e4a0ec384ff5ec13f0e1a525014ae96ab282dd09
Kawai-Senpai <bhowmickranitking@duck.com>
2026-01-30 20:52:06 +0530

    feat: Bump version to 7.1.0 in setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (this_directory / "README.md").read_text(encoding='utf-8')
 
 setup(
     name='ultragpt',
-    version='7.0.0',
+    version='7.1.0',
     license="MIT",
     author='Ranit Bhowmick',
     author_email='mail@ranitbhowmick.com',
